edf7e3aa5edb71384df671a4150b66780a46bb05
Robert Sparks <robert.william.sparks@gmail.com>
2025-03-13 21:34:36 +0000

    removed custom nix build jank

--- flake.nix
+++ flake.nix
@@ -14,60 +14,14 @@
       devShells = forAllSystems (system:
         let
           pkgs = import nixpkgs { inherit system; };
-          
-          # Create tsconfig.json
-          tsconfigFile = pkgs.writeText "tsconfig.json" ''
-            {
-              "compilerOptions": {
-                "target": "ES2020",
-                "lib": ["ES2020", "DOM"],
-                "strict": true,
-                "moduleResolution": "node",
-                "esModuleInterop": true,
-                "skipLibCheck": true,
-                "forceConsistentCasingInFileNames": true,
-                "outDir": "./docs"
-              },
-              "include": ["src/**/*"],
-              "exclude": ["node_modules"]
-            }
-          '';
-
-          # Updated build script that uses tsconfig.json
-          buildScript = pkgs.writeShellScriptBin "build" ''
-            ${pkgs.typescript}/bin/tsc -p tsconfig.json
-            cp src/*.html docs/
-          '';
-
-          # Updated watch script that uses tsconfig.json
-          watchScript = pkgs.writeShellScriptBin "watch" ''
-            ${pkgs.typescript}/bin/tsc -p tsconfig.json --watch
-          '';
-
-          cleanScript = pkgs.writeShellScriptBin "clean" ''
-            rm -rf docs/*
-            rm -f tsconfig.json
-          '';
-
-          serveScript = pkgs.writeShellScriptBin "serve" ''
-            echo "Serving on http://localhost:8000"
-            ${pkgs.python3}/bin/python -m http.server 8000 --directory docs/
-          '';
         in
         with pkgs; {
           default = mkShell {
             buildInputs = [
               typescript
-              python3
-              buildScript
-              cleanScript
-              watchScript
-              serveScript
+	      corepack
+	      nodejs
             ];
-            shellHook = ''
-              mkdir -p docs
-              cp ${tsconfigFile} tsconfig.json
-            '';
           };
         }
       );
