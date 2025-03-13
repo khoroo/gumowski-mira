5268c518138acafde6f5347b7115a1bc086f6f25
Robert Sparks <robert.william.sparks@gmail.com>
2025-03-13 22:41:43 +0000

    changed root url to /gumorski-mira

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: '.',
+  base: '/gumowski-mira/', // Add this line to specify the subdirectory path
   server: {
     port: 3000,
   },
